0c6c58c66871378009dee191d3687e664948e669
Michael Weinberg <mwweinberg@users.noreply.github.com>
2019-12-24 12:06:45 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,79 @@
+
+
+
+
+//https://openaccess-api.clevelandart.org/api/artworks/?skip=5&limit=10&indent=1&cc0=1&has_image=1&department=Chinese%20Art
+
+//function to generate a random number
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+//uses the function to pick a random image in the collection
+var offset = getRndInteger(1, 31278);
+//inserts that random number into the request url, returning a json file
+var target_json_url = "https://openaccess-api.clevelandart.org/api/artworks/?limit=10&indent=1&cc0=1&has_image=1&skip=" + offset;
+
+console.log(target_json_url);
+
+//create new request object instance
+let request = new XMLHttpRequest();
+//opens the file
+request.open('GET', target_json_url);
+request.responseType = 'json'
+request.send();
+
+request.onload = function() {
+    const response_json = request.response;
+    //gets the image URL + tombstone of a random image from the collection and turns it into an array assigned to a variable
+    var found_image_info = grabImageInfo(response_json);
+
+
+
+    console.log('this ' + found_image_info[0]);
+    console.log('that ' + found_image_info[1]);
+
+    var picked_image_URL = found_image_info[0];
+    var picked_image_tombstone = found_image_info[1];
+
+    //creates the image to be  posted
+    var img = document.createElement("img");
+    img.src = picked_image_URL;
+    //img.width = 100;
+    //img.height = 100;
+    img.alt = 'picked_image_tombstone';
+
+    //creates the text
+    var tomb_text = document.createTextNode(picked_image_tombstone)
+
+    //creates the linebreak
+    var linebreak = document.createElement('br');
+
+
+    // This next line will just add the image and tombstone to the <body> tag
+    // document.body.appendChild(img);
+    // document.body.appendChild(linebreak);
+    // document.body.appendChild(tomb_text);
+
+
+    let item = document.createElement('div');
+    item.classList.add('item');
+    item.innerHTML = `<div class="container"><img class="beach-image"  src="${picked_image_URL}" alt="beach image"/><div class="textStyle">${picked_image_tombstone}</div></div>`;
+    document.body.appendChild(item);
+
+
+}
+
+function grabImageInfo(jsonObj) {
+    console.log(jsonObj['data'][0]['images']['web']['url']);
+    //creates an array with the URL and tombstone of the random object picked
+    var function_image_url = [jsonObj['data'][0]['images']['web']['url'], jsonObj['data'][0]['tombstone']];
+    console.log(function_image_url[0]);
+    //returns that array
+    return function_image_url;
+}
+
+
+
+
+
